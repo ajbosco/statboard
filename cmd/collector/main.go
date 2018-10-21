6ecceddcd3e4e879563f75a799624e14790f2463
Adam Boscarino <ajboscarino@gmail.com>
2018-10-20 22:52:39 -0400

    use logrus

--- cmd/collector/main.go
+++ cmd/collector/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/ajbosco/statboard/pkg/collector"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
 )
 
 // Config contains environment variables for the metric collector
@@ -21,28 +21,28 @@ func main() {
 	var cfg Config
 	err := envconfig.Process("statboard", &cfg)
 	if err != nil {
-		log.Fatal(err.Error())
+		logrus.Fatal(err.Error())
 	}
 
 	c, err := createCollector(cfg.CollectorType, cfg.ConfigFilePath)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 	s, err := collector.NewBoltStore(cfg.DbFilePath)
 	if err != nil {
-		log.Fatal(err)
+		logrus.Fatal(err)
 	}
 
 	metricName := cfg.MetricName
 	metrics, err := c.Collect(metricName, 10)
 	if err != nil {
-		log.Fatal(errors.Wrap(err, fmt.Sprintf("failed to collect metric:%q", metricName)))
+		logrus.Fatal(errors.Wrap(err, fmt.Sprintf("failed to collect metric:%q", metricName)))
 	}
 
 	for _, met := range metrics {
 		err = s.WriteMetric(met)
 		if err != nil {
-			log.Fatal(errors.Wrap(err, fmt.Sprintf("failed to write metric:%q", metricName)))
+			logrus.Fatal(errors.Wrap(err, fmt.Sprintf("failed to write metric:%q", metricName)))
 		}
 	}
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ require (
 	github.com/etcd-io/bbolt v1.3.0
 	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/pkg/errors v0.8.0
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/sajal/fitbitclient v0.0.0-20160707181904-9e74ca7630a5
+	github.com/sirupsen/logrus v1.1.1
 	github.com/spf13/viper v1.2.1
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/net v0.0.0-20181017193950-04a2e542c03f // indirect
